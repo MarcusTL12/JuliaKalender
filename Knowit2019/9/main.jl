93f39d58e31c193304c14266454e146abe4b5010
MarcusTL12 <marcuspaafjellet@gmail.com>
2019-12-09 17:23:49 +0100

    more testing

--- Knowit2019/9/main.jl
+++ Knowit2019/9/main.jl
@@ -1,5 +1,5 @@
 
-function iscrampit(n)
+function iskrampus(n)
     i = 10
     n2 = n^2
     while (a = n2 ÷ i) != 0
@@ -11,10 +11,32 @@ function iscrampit(n)
     0
 end
 
+function iskrampus2(n)
+    i = 10
+    while n^2 ÷ i != 0
+        if n^2 ÷ i + n^2 % i == n && n^2 ÷ i != 0 && n^2 % i != 0
+            return n
+        end
+        i *= 10
+    end
+    0
+end
+
 
 function main()
     open(
-        io->mapreduce(x->iscrampit(parse(Int, x)), +, eachline(io)),
+        io->mapreduce(x->iskrampus2(parse(Int, x)), +, eachline(io)),
         "krampus.txt", "r"
     )
+end
+
+
+function main2()
+    open("krampus.txt", "r") do io
+        s = 0
+        for l in eachline(io)
+            s += iskrampus(parse(Int, l))
+        end
+        s
+    end
 end
