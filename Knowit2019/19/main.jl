e0471e0f006d0d61d10d51fc4f20a21dad9d0e07
Marcus Lexander <marcuspaafjellet@gmail.com>
2019-12-19 15:38:27 +0100

    19 opt

--- Knowit2019/19/main.jl
+++ Knowit2019/19/main.jl
@@ -1,12 +1,42 @@
 
-palindrome(n) = (s = string(n)) == reverse(s)
+
+amtdigits(n) = floor(Int, log10(n) + 1)
+
+
+function reversenum(n)
+    d = amtdigits(n)
+    
+    nnum = 0
+    
+    for i in 1 : d
+        nnum *= 10
+        nnum += n % 10
+        n ÷= 10
+    end
+    
+    nnum
+end
+
+
+palindrome(n) = n == reversenum(n)
 
 
 function hiddenpalindrome(n)
-    palindrome(n + parse(Int, reverse(string(n)))) && !palindrome(n)
+    palindrome(n + reversenum(n)) && !palindrome(n)
 end
 
 
 function main()
     sum(filter(hiddenpalindrome, 1 : 123454321))
+end
+
+
+function main2()
+    acc = 0
+    for i in 1 : 123454321
+        if hiddenpalindrome(i)
+            acc += i
+        end
+    end
+    acc
 end
